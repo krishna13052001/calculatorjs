6fa866010de9770fe380cf3a22bfe92ace94d320
krishna13052001 <sathya13052001krishna@gmail.com>
2021-06-05 19:39:16 +0530

    inital update

---
+++ assets/scripts/app.js
@@ -0,0 +1,88 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+function add(){
+    const calcDescription = `${currentResult} + ${parseInt(userInput.value)}`;
+    currentResult = currentResult+parseInt(userInput.value)
+    userInput.value=null;
+    if(Number.isNaN(currentResult)){
+        outputResult("invalid operends", '');
+        currentResult = 0
+        return;
+    } else{
+        console.log(currentResult)
+        outputResult(currentResult, calcDescription);    
+    }
+}
+
+function sub(){
+    const calcDescription = `${currentResult} - ${parseInt(userInput.value)}`;
+    currentResult = currentResult-parseInt(userInput.value)
+    userInput.value=null;
+    if(Number.isNaN(currentResult)){
+        outputResult("invalid operends", '');
+        currentResult = 0
+        return;
+    }
+    outputResult(currentResult, calcDescription);
+}
+
+function mul(){
+    const calcDescription = `${currentResult} * ${parseInt(userInput.value)}`;
+    currentResult = currentResult*parseInt(userInput.value)
+    userInput.value=null;
+    if(Number.isNaN(currentResult)){
+        outputResult("invalid operends", '');
+        currentResult = 0
+        return;
+    }
+    outputResult(currentResult, calcDescription);
+}
+
+function div(){
+    const calcDescription = `${currentResult} / ${parseInt(userInput.value)}`;
+    currentResult = currentResult/parseInt(userInput.value)
+    userInput.value=null;
+    if(Number.isNaN(currentResult)){
+        outputResult("invalid operends", '');
+        currentResult = 0
+        return;
+    }
+    outputResult(currentResult, calcDescription);
+}
+
+function rem(){
+    const calcDescription = `${currentResult} % ${parseInt(userInput.value)}`;
+    currentResult = currentResult%parseInt(userInput.value)
+    userInput.value=null;
+    if(Number.isNaN(currentResult)){
+        outputResult("invalid operends", '');
+        currentResult = 0
+        return;
+    }
+    outputResult(currentResult, calcDescription);
+}
+
+function pow(){
+    const calcDescription = `${currentResult} ^ ${parseInt(userInput.value)}`;
+    currentResult = currentResult**parseInt(userInput.value)
+    userInput.value=null;
+    if(Number.isNaN(currentResult)){
+        outputResult("invalid operends", '');
+        currentResult = 0
+        return;
+    }
+    outputResult(currentResult, calcDescription);
+}
+
+function clear(){
+    currentResult = 0;
+    outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',sub);
+multiplyBtn.addEventListener('click',mul);
+divideBtn.addEventListener('click',div);
+reminderBtn.addEventListener('click',rem);
+powerBtn.addEventListener('click',pow);
+clearBtn.addEventListener('click',clear);
